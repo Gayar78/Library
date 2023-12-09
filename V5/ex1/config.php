862bfe3dfc8921b5fb10d1746d81f69ec533b59e
Gayar <132988567+Gayar78@users.noreply.github.com>
2023-12-09 23:15:14 +0100

    Library

--- V5/ex1/config.php
+++ V5/ex1/config.php
@@ -7,9 +7,9 @@
     <body>
         <?php 
             define("HOSTNAME", "localhost");
-            define("DATABASE", "rthiba2");
-            define("LOGIN","rthiba2");
-            define ("PASSWORD",",D4Gw$&fYh^7SZOjUu=i");
+            define("DATABASE", "database");
+            define("LOGIN","login");
+            define ("PASSWORD","password");
         ?>
     </body>
 </html>
