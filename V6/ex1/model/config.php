977f02316e2306c8f46532cba292511df9f1aed2
Gayar <132988567+Gayar78@users.noreply.github.com>
2023-12-09 23:21:14 +0100

    Library

--- V6/ex1/model/config.php
+++
@@ -1,6 +0,0 @@
-        <?php 
-            define("HOSTNAME", "localhost");
-            define("DATABASE", "rthiba2");
-            define("LOGIN","rthiba2");
-            define ("PASSWORD",",D4Gw$&fYh^7SZOjUu=i");
-        ?>
